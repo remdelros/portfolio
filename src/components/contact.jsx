3f3da568e506b2c868e39cb87f145a47ec8a805b
remodelros <remonster510@gmail.com>
2024-10-06 15:15:15 +0800

    few changes

--- src/components/contact.jsx
+++ src/components/contact.jsx
@@ -1,23 +1,22 @@
 const Contact = () => {
-    return (
-      <div className="border-b border-neutral-900 pb-4">
-        <h1 className="my-20 text-center text-4xl">Contact Me</h1>
-        <p className="text-center text-neutral-400 mb-8">
-          Feel free to reach out to me through the following contact information:
-        </p>
-        <div className="flex flex-col items-center justify-center">
-          <div className="mb-4 text-neutral-300 text-center">
-            <h2 className="text-xl font-bold">Email:</h2>
-            <p className="text-lg">mappataorem@gmail.com</p>
-          </div>
-          <div className="text-neutral-300 text-center">
-            <h2 className="text-xl font-bold">Phone:</h2>
-            <p className="text-lg">+639911675416</p>
-          </div>
+  return (
+    <div className="border-b border-neutral-900 pb-4">
+      <h1 className="my-3 text-center text-4xl">Contact Me</h1>
+      <p className="text-center text-neutral-400 mb-6">
+        Feel free to reach out to me through the following contact information:
+      </p>
+      <div className="flex flex-col items-center justify-center space-y-4">
+        <div className="text-neutral-300 text-center">
+          <h2 className="text-xl font-bold mb-1">Email:</h2>
+          <p className="text-lg">mappataorem@gmail.com</p>
+        </div>
+        <div className="text-neutral-300 text-center">
+          <h2 className="text-xl font-bold mb-1">Phone:</h2>
+          <p className="text-lg">+639911675416</p>
         </div>
       </div>
-    );
-  };
-  
-  export default Contact;
-  
+    </div>
+  );
+};
+
+export default Contact;
